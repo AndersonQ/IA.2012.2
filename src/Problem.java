9cd62b8b858dc9dccded70e71c86a1906b74cc58
Anderson de França Queiroz <contato@andersonq.eti.br>
2012-10-10 17:01:28 -0300

    Removido default package, usar package ia

--- src/Problem.java
+++
@@ -1,8 +0,0 @@
-import java.util.List;
-
-
-public interface Problem 
-{
-	List<Object> expand(Object o);
-	Result process(Object o);
-}
--- src/Result.java
+++
@@ -1,6 +0,0 @@
-
-public class Result 
-{
-	private boolean success;
-
-}
