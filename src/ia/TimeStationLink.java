f681af17da6ad60f591be53de3c5ac4b6ac58154
Tiago de França Queiroz <contato@tiago.eti.br>
2012-10-18 09:46:18 -0300

    Classes to go from UFABC to USP with time cost

---
+++ src/ia/TimeStationLink.java
@@ -0,0 +1,46 @@
+package ia;
+
+import org.jgrapht.UndirectedGraph;
+import org.jgrapht.graph.DefaultEdge;
+
+public class TimeStationLink extends DefaultEdge 
+{
+	private static int count = 0;
+	protected UndirectedGraph<Station, TimeStationLink> graph;
+	public int my_number;
+	protected double time;
+	
+	public TimeStationLink(UndirectedGraph<Station, TimeStationLink> graph, double time)
+	{
+		this.my_number = this.count++;
+		this.graph = graph;
+	}
+	
+	@Override
+	protected Station getSource()
+	{
+		return graph.getEdgeSource(this);
+	}
+
+	@Override
+	protected Station getTarget()
+	{
+		return graph.getEdgeTarget(this);
+	}
+
+	@Override
+	public String toString()
+	{
+		return new String ("N:" + my_number + " " + this.getSource() + " <-- " + time + " --> " + this.getTarget());
+	}
+
+	public double getTime()
+	{
+		return time;
+	}
+
+	public void setTime(double time)
+	{
+		this.time = time;
+	}
+}
