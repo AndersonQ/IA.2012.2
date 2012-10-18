63db8cff29416652d6f496d8d1c8b0393f76d91e
Anderson de França Queiroz <contato@andersonq.eti.br>
2012-10-18 09:56:07 -0300

    Developing

--- src/ia/DeepSearch.java
+++ src/ia/DeepSearch.java
@@ -1,18 +1,54 @@
 package ia;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
+import java.util.Stack;
+
+import org.jgrapht.UndirectedGraph;
 
 public class DeepSearch implements SearchProblem {
 
+	/** The graph */
+	private UndirectedGraph<City, WeightCityLink> graph;
+	/** A stack to walk trough graph */ 
+	private Stack<City> stack;
+	
+	/**
+	 * Deep search to search problem
+	 * @param graph
+	 */
+	public DeepSearch(UndirectedGraph<City, WeightCityLink> graph)
+	{
+		this.graph = graph;
+		this.stack = new Stack<City>();
+	}
+	
 	@Override
 	public List<Object> expand(Object o) {
-		// TODO Auto-generated method stub
-		return null;
+		//Cast to city
+		City city = (City) o;
+		//A Set to keep neighbour
+		Set<WeightCityLink> neighbour;
+		//Get neighbours
+		neighbour = graph.edgesOf(city);
+		
+		List<Object> l = new LinkedList<Object>();
+		
+		for(WeightCityLink c : neighbour)
+		{
+			if(!l.contains(c.getTarget()))
+			{
+				l.add(c);
+			}
+		}
+		
+		return l;
 	}
 
 	@Override
 	public SearchResult process(Object o) {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
