3995c8081bd39b06885a18f537c52498aefbf774
Anderson de França Queiroz <contato@andersonq.eti.br>
2012-10-18 08:42:11 -0300

    Main created

---
+++ src/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Main {
+
+	/** Main function
+	 * @param args arguments
+	 */
+	public static void main(String[] args) {
+		System.out.println("It works xD");
+
+	}
+
+}
