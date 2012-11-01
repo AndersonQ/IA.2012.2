b09e08098a2889ecd9414f684725925461460e73
Tiago de França Queiroz <contato@tiago.eti.br>
2012-11-01 09:56:12 -0200

    Expand and constructor finished

--- src/ia/GreedySearch.java
+++ src/ia/GreedySearch.java
@@ -3,6 +3,8 @@ package ia;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 
@@ -11,15 +13,16 @@ import org.jgrapht.UndirectedGraph;
 public class GreedySearch  implements SearchProblem
 {
     private UndirectedGraph<City, WeightCityLink> g;
-    private City destiny;
+    private City destiny, source;
     private double h[][];
     private List<String> cities;
 
-    public GreedySearch(UndirectedGraph<City, WeightCityLink> g, City destiny, double h[][])
+    public GreedySearch(UndirectedGraph<City, WeightCityLink> g, City source, City destiny, double h[][])
     {
         Set<City> vc;
         this.g = g;
         this.destiny = destiny;
+        this.source = source;
         this.h = h;
 
         cities = new ArrayList<String>();
@@ -29,11 +32,61 @@ public class GreedySearch  implements SearchProblem
             cities.add(c.getName());
 
         Collections.sort(cities);
+
+        System.out.printf("Cities in order:\n");
+        for(String c: cities)
+            System.out.println(c);
     }
 
     public List<Object> expand(Object o)
     {
-        return null;
+        List<Object> ret;
+        List<City> sorted;
+        Set<WeightCityLink> s;
+        City node = (City) o;
+        City sl, dl;
+
+        ret = new LinkedList<Object>();
+        sorted = new LinkedList<City>();
+
+        s = g.edgesOf(node);
+        for(WeightCityLink l: s)
+        {
+            sl = l.getSource();
+            dl = l.getTarget();
+            if(sl != node)
+                if(!sorted.contains(sl))
+                    sorted.add(sl);
+            if(dl != node)
+                if(!sorted.contains(dl))
+                    sorted.add(dl);
+        }
+
+        Collections.sort(sorted, new Comparator<City>()
+        {
+            public int compare(City arg0, City arg1)
+            {
+                double sd, ds;
+                sd = getHeuristic(arg0);
+                ds = getHeuristic(arg1);
+
+                if(sd > ds)
+                    return 1;
+                else if (ds > ds)
+                    return -1;
+                else
+                    return 0;
+            }
+        });
+
+        System.out.println("Nodes sorted by heuristic:");
+        for(City c: sorted)
+        {
+            System.out.println(c);
+            ret.add((Object) c);
+        }
+
+        return ret;
     }
 
     public SearchResult process(Object o)
@@ -41,12 +94,12 @@ public class GreedySearch  implements SearchProblem
         return null;
     }
 
-    private double getHeuristic(City src, City dest)
+    private double getHeuristic(City src)
     {
         int s, d;
 
         s = cities.indexOf(src.getName());
-        d = cities.indexOf(dest.getName());
+        d = cities.indexOf(destiny.getName());
 
         return h[s][d];
     }
