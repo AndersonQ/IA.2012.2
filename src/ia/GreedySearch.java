0893cb2b1ff37b2b8f304a1ad97188ee1a199ac7
Tiago de França Queiroz <contato@tiago.eti.br>
2012-11-01 08:37:00 -0200

    Implements SearchProblem

---
+++ src/ia/GreedySearch.java
@@ -0,0 +1,18 @@
+package ia;
+
+import java.util.List;
+
+public class GreedySearch  implements SearchProblem
+{
+
+    public List<Object> expand(Object o)
+    {
+        return null;
+    }
+
+    public SearchResult process(Object o)
+    {
+        return null;
+    }
+
+}
