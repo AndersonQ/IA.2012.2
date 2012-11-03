26d362d281435d1dc8de8bb14f0940befff12362
Anderson de França Queiroz <contato@andersonq.eti.br>
2012-11-03 20:27:01 -0200

    Imports

--- src/ia/tests/GreedyTests.java
+++ src/ia/tests/GreedyTests.java
@@ -1,8 +1,10 @@
 package ia.tests;
 
 import ia.City;
+import ia.Colour;
 import ia.WeightCityLink;
 import ia.search.GreedySearch;
+import ia.search.SearchResult;
 
 import java.util.List;
 import java.util.Stack;
