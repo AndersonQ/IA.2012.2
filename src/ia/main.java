8bca6b57b725c0337ea633a0ab12edbc55f6ee6e
Tiago de França Queiroz <contato@tiago.eti.br>
2012-11-07 21:44:56 -0200

    .

--- src/ia/main.java
+++ src/ia/main.java
@@ -1,14 +1,24 @@
 package ia;
 
+import ia.search.ExecuteGreedy;
+
 public class main
 {
-
     /**
      * @param args
      */
     public static void main(String[] args)
     {
+        ExecuteGreedy gs = new ExecuteGreedy();
 
+        try
+        {
+            gs.run(1, 2);
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+            System.exit(1);
+        }
     }
-
 }
