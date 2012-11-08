2521cefa5cb14c68af73570bd96b71d2e7f0c354
Tiago de França Queiroz <contato@tiago.eti.br>
2012-11-08 09:10:36 -0200

    run

--- src/ia/tests/ExecuteBreathStation.java
+++ src/ia/tests/ExecuteBreathStation.java
@@ -43,8 +43,8 @@ public class ExecuteBreathStation {
     public ExecuteBreathStation(){
     }
 
-    public static void main(String[] args){
-
+    public void run()
+    {
         UndirectedGraph<Station, TimeStationLink> g = new SimpleGraph<Station, TimeStationLink>(TimeStationLink.class);
         Station e1,e2,e3,e4,e5,e6,e7,e8,e9,e10,e11,e12,e13,e14,e15,e16;
 
